f8178b1429681d7eb208cbc563d84260d165277a
Version Upgrade <n/a>
2026-01-21 08:47:55 +0000

    Upgrade to .NET 8 on Iteration 2

--- Application/ServiceExtensions.cs
+++ Application/ServiceExtensions.cs
@@ -17,9 +17,10 @@ namespace Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            services.AddMediatR(Assembly.GetExecutingAssembly());
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            });
         }
     }
 }
