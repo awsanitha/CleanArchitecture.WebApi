ad3f8dd55d0c32e97c7b17454c85742fe2dff17f
Version Upgrade <n/a>
2026-06-29 03:33:30 +0000

    Upgrade to .NET 10 on Iteration 4

--- Application/Features/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
+++ Application/Features/Products/Commands/CreateProduct/CreateProductCommandValidator.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Interfaces.Repositories;
 using Domain.Entities;
 using FluentValidation;
-using Microsoft.EntityFrameworkCore.Internal;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
