afcafe3e5039d00e66083fa6211a73d9cd935c41
Version Upgrade <n/a>
2026-07-12 07:37:40 +0000

    Upgrade to .NET 10 on Iteration 2

--- WebApi/Extensions/ServiceExtensions.cs
+++ WebApi/Extensions/ServiceExtensions.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApi.Extensions
 {
@@ -54,6 +52,7 @@ namespace WebApi.Extensions
                 });
             });
         }
+
         public static void AddApiVersioningExtension(this IServiceCollection services)
         {
             services.AddApiVersioning(config =>
